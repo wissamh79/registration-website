fed2661b0d0b7464f3941fa19b5d7d88bd3fed52
Wissam  Hassan <wsamh797@gmail.com>
2023-08-04 09:29:39 +0300

    fix re

--- app/registration/page.jsx
+++ app/registration/page.jsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import { useToast } from "@/components/ui/use-toast";
+import { BASE_URL } from "@/utils/urls";
 export default function Registration() {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
